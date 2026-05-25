e08493d40b6e489ad1e34b7d502a5ed065722e47
Agustin Rahmat Tulloh <rahmattulloh323@gmail.com>
2026-05-25 22:07:45 +0700

    folder templating

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
 import './bootstrap';
+
+window.onscroll = function () {
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add("navbar-fixed");
+    } else {
+        header.classList.remove("navbar-fixed");
+    }
+};
+
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+});
+
+function toggleContent(postId) {
+    const content = document.getElementById("content" + postId);
+    const fullContent = document.getElementById("full-content" + postId);
+
+    if (fullContent.style.display === "none") {
+        fullContent.style.display = "block";
+        content.style.display = "none";
+    } else {
+        fullContent.style.display = "none";
+        content.style.display = "block";
+    }
+}
