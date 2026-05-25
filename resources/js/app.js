ec7c4ec9673c044613ce0ca5e27850549d7b4e43
denoyey <ddeblong42@gmail.com>
2026-05-25 19:38:06 +0700

    add: tambah map.js dan swiper.js di js/components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import { initHeroSwiper } from './components/swiper';
+import { initMap } from './components/map';
+
+// Jalankan semua script inisialisasi saat DOM sudah siap dimuat
+document.addEventListener('DOMContentLoaded', () => {
+    initHeroSwiper();
+    initMap();
+});
---
+++ resources/js/components/swiper.js
@@ -0,0 +1,12 @@
+// resources/js/components/swiper.js
+
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export function initHeroSwiper() {
+    // disini untuk masukin code untuk swiper
+}
